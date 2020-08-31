89abecb9f550041b1a021061b06eb868318aa223
Abhishek <70463156+abh1shekgupta@users.noreply.github.com>
2020-09-01 02:26:55 +0530

    Main files

---
+++ app.js
@@ -0,0 +1,18 @@
+function IncSizehtml() {
+    document.getElementById('langname').innerHTML = "<p style=\"color:white\">HTML<p>";
+}
+function IncSizecss() {
+    document.getElementById('langname').innerHTML = "<p style=\"color:white\">CSS<p>";
+}
+function IncSizejs() {
+    document.getElementById('langname').innerHTML = "<p style=\"color:white\">JAVASCRIPT<p>";
+}
+function IncSizepy() {
+    document.getElementById('langname').innerHTML = "<p style=\"color:white\">PYTHON<p>";
+}
+function IncSizecpp() {
+    document.getElementById('langname').innerHTML = "<p style=\"color:white\">C++<p>";
+}
+function reset() {
+    document.getElementById('langname').innerHTML = " ";
+}
